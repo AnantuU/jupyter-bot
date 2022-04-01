c5d515891bcef9cc643a23919c2f2a5b3bfc54d8
AnantuU <88704238+AnantuU@users.noreply.github.com>
2022-04-01 22:41:33 +0530

    Update main.py
    added slash command for ping command and added 8ball command

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 
+
+		
 import discord
 
 import random
 
+
+
 import os
 
-
+import keep_alive
 
 from dotenv import load_dotenv
+
+from discord_slash import SlashCommand
 
 
 from discord.ext import commands
@@ -20,6 +26,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 
 bot = commands.Bot(command_prefix="+")
+slash = SlashCommand(bot, sync_commands = True)
 @bot.event
 async def on_ready():
   await bot.change_presence(status=discord.Status.idle, activity=discord.Game('Jupyter'))
@@ -44,11 +51,21 @@ async def on_message(message):
 
 	await bot.process_commands(message)
 
+
+      
+
+    
+    
+  
+
+  
+
   
 @bot.command()
 async def coolrate(ctx):
   embed = discord.Embed(title="CoolRate", description = f"You are {random.randrange(100)}% cool {ctx.author.mention}", color=discord.Color.random())
   await ctx.send(embed = embed)
+
 
 
 @bot.command()
@@ -88,5 +105,51 @@ async def print(ctx, *args):
 
 	await ctx.channel.send(response)
 
+@bot.command(aliases=['8ball'])
+async def eightball(ctx,*,question):
+ responses  = ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful.",
+                "Maybe."]
+ await ctx.send(f':8ball: Answer: {random.choice(responses)}')  
+
+
+ 
+
+
+    
+  
+
+
+
+
+@slash.slash(name="Ping", description="Ping Command")
+async def Ping(ctx):
+              
+
+	await ctx.send("pong")
+
+
+
+
+
 
 bot.run(DISCORD_TOKEN)
+
